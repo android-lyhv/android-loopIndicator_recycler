af53cec162f82b45d93340d2a24e98a461d14c7e
lyhv <lyhv@tech.est-rouge.com>
2019-08-22 09:06:52 +0700

    Add final modifiler

--- LoopIndicatorLibrary/src/main/java/com/lyhv/library/CycleFragmentStatePagerAdapter.kt
+++ LoopIndicatorLibrary/src/main/java/com/lyhv/library/CycleFragmentStatePagerAdapter.kt
@@ -8,14 +8,17 @@ import androidx.fragment.app.FragmentStatePagerAdapter
  * Created by lyhv on August 15, 2019
  * Copyright @ est-rouge. All rights reserved
  */
-abstract class CycleFragmentStatePagerAdapter(fm: FragmentManager, private var loopCount: Int = IndicatorConfig.LOOP_COUNT) : FragmentStatePagerAdapter(fm) {
+abstract class CycleFragmentStatePagerAdapter(
+    fm: FragmentManager,
+    private var loopCount: Int = IndicatorConfig.LOOP_COUNT
+) : FragmentStatePagerAdapter(fm) {
     abstract fun getRealItemSize(): Int
     abstract fun getRealItem(position: Int): Fragment
-    override fun getItem(position: Int): Fragment {
+    final override fun getItem(position: Int): Fragment {
         return getRealItem(position.rem(getRealItemSize()))
     }
 
-    override fun getCount(): Int {
+    final override fun getCount(): Int {
         return getRealItemSize() * loopCount
     }
 }
